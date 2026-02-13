85f2b063ab2f3c4cddce7875740de62124c22ce9
Edo-ado <edu18088@gmail.com>
2026-02-13 08:49:56 -0600

    Agregar archivos de proyecto.

---
+++ D&A.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using D_A.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D_A.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ D&A.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace D_A.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
