6ce4ec674c743c45a3af7456e7ef7a97d891836e
Ashkituru <assibajaro@est.utn.ac.cr>
2026-02-21 23:28:33 -0600

    lista usuarios (falta pulir)

---
+++ DungeonsAndAuctions.Web/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using D_A.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNDA.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IServiceUser _serviceUser;
+        public UserController(IServiceUser serviceUser)
+        {
+            _serviceUser = serviceUser;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var collection = await _serviceUser.ListAsync();
+            return View(collection);
+        }
+    }
+}
--- DungeonsAndAuctions.Web/Program.cs
+++ DungeonsAndAuctions.Web/Program.cs
@@ -1,29 +1,47 @@
+using D_A.Application.Services.Implementations;
+using D_A.Application.Services.Interfaces;
+using D_A.Infraestructure.Data;
+using D_A.Infraestructure.Repository.Implementation;
+using D_A.Infraestructure.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Controllers y Views
 builder.Services.AddControllersWithViews();
+
+//Repositories
+builder.Services.AddScoped<IRepositoryUser, RepositoryUser>();
+
+//Services
+builder.Services.AddScoped<IServiceUser, ServiceUser>();
+
+//AutoMapper
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile<UserProfile>();
+});
+
+//DbContext
+var connectionString = builder.Configuration.GetConnectionString("SqlServerDataBase");
+builder.Services.AddDbContext<DAContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
